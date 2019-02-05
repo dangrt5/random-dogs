7b9f68a9524fa2460755605bce62f4eaaa5409e4
dangrt5 <randy.dang05@gmail.com>
2019-02-05 15:43:09 -0800

    header component complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from "./Header";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <h1 className="text-center">Header</h1>
+        <Header />
         <h1 className="text-center">Body</h1>
         <h1 className="text-center">Footer</h1>
       </div>
