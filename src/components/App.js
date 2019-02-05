d87b217fb4d436a175204b1e0fc4b850e4f94018
dangrt5 <randy.dang05@gmail.com>
2019-02-05 15:02:53 -0800

    Added bootstrap cdn

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,10 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <h1 className="text-center">Header</h1>
+        <h1 className="text-center">Body</h1>
+        <h1 className="text-center">Footer</h1>
       </div>
     );
   }
