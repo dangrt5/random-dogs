617c04cbc836bf7980f66fbb6a6921af92c5b974
dangrt5 <randy.dang05@gmail.com>
2019-02-05 19:21:41 -0800

    Feature: footer edit functionality added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,27 @@ import React from "react";
 import "../assets/css/Footer.css";
 
 export default class Footer extends React.Component {
+
+    state = {
+        input: '',
+    }
+
+
+    handleInput = e => {
+        const { value } = e.currentTarget.value;
+        this.setState({ input: value })
+    }
+
+
     render() {
+        const { input } = this.state;
+
         return (
             <div className="footer row">
                 <form>
                     <p>Select # of facts (1-4)</p>
                     <div className="form-group">
-                        <input type="text" placeholder="0" />
+                        <input onChange={this.handleInput} type="text" value={input} />
                     </div>
                     <button className='btn btn-lg btn-primary'>Generate</button>
                 </form>
