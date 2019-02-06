ea806111f6540e547abd716a7ce8c73f17ee3ba1
apark105 <andyjpark105@gmail.com>
2019-02-05 19:20:26 -0800

    Added the DogFacts Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import DogFacts from './dogFacts'; 
+import DogPics from './DogPics'; 
 import Header from "./Header";
 import Footer from "./Footer";
+import DogFacts from './DogFacts';
 
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="container">
         <Header />
         <DogFacts/>
+        <DogPics/>
         <Footer />
       </div>
     );
--- src/components/dogFacts.js
+++ src/components/dogFacts.js
@@ -1,57 +1,14 @@
-import React, { Component } from 'react'; 
-import '../assets/dogFacts.css';
-import axios from 'axios';
+import React, { Component } from 'react';
+import '../assets/css/DogFacts.css';
 
 class DogFacts extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            dogObj: [],
-        }
-
-    }
-    componentDidMount = async ()=> {
-       await axios.get("https://dog.ceo/api/breeds/image/random/4").then( (resp) => {
-            console.log(resp.data.message);
-            const dogImgArray = resp.data.message;
-            const dogArray = [];
-            dogImgArray.map( (item, index)=> {
-                let num = 30;
-                let dogName = '';
-                while(item[num] != '/') {
-                    dogName += item[num]; 
-                    num++;
-                }
-                dogArray.push({dog: dogName, img: item});
-            })
-            console.log(dogArray);
-            this.setState({
-                dogObj: dogArray,
-            })
-       } )
-    }
-
     render() {
-        console.log('render', this.state.dogObj)
-        const individualDog = this.state.dogObj.map( (item, index) => {
-             return (
-                    <div className="col-sm-12 col-md-6 col-lg-3">
-                        <div className="individualCards">
-                            <div className="col-sm-12 text-center cardTitle">{item.dog}</div>
-                            <img src={item.img} className="col-sm-12 cardImg"/>
-                            <div className="col-sm-12 text-center cardFact">Facts Facts Facts</div>
-                        </div>
-                    </div>
-                )
-        })
         return (
-            <div className="container">   
-                <div className="row">
-                   {individualDog}
+                <div className="randomText text-center">
+                    Random Fact: 
                 </div>
-            </div>
         )
     }
 }
 
-export default DogFacts; 
+export default DogFacts;
